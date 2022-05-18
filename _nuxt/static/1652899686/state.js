c9e4b13ebc0246b7b7e404046ab18721ff5914d7
Guillermo Croppi <guillermocroppi@gmail.com>
2022-05-18 15:48:09 -0300

    b46b5a6

---
+++ _nuxt/static/1652899686/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Faccompany-map\u002F_nuxt\u002Fstatic\u002F1652899686",layout:"default",error:a,state:{sheet:{data:a}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Faccompany-map\u002F",assetsPath:"\u002Faccompany-map\u002F_nuxt\u002F",cdnURL:a}}}}(null));
